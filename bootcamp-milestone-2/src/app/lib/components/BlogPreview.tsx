e0ee3e20d66d25f6bb360d6ee543df5ade6f1b0d
snek152 <kakanisnehil@gmail.com>
2025-12-11 19:10:40 -0800

    fixing type checking

--- bootcamp-milestone-2/src/app/lib/components/BlogPreview.tsx
+++ bootcamp-milestone-2/src/app/lib/components/BlogPreview.tsx
@@ -22,10 +22,7 @@ export default function BlogPreview({
       />
       <div className="flex-1">
         <h2 className="m-0 mb-2 text-xl font-semibold text-white">{title}</h2>
-        <time
-          dateTime={typeof date === "string" ? date : date.toISOString()}
-          className="block text-gray-400 text-sm mb-2"
-        >
+        <time dateTime={date} className="block text-gray-400 text-sm mb-2">
           {new Date(date).toLocaleDateString("en-US", {
             year: "numeric",
             month: "long",
